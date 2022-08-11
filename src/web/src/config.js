1d244b40ecc0f5ed2f7ab4ddecfb954a5a3c5dec
Max Parker <maxrparker@gmail.com>
2022-08-11 15:03:33 -0600

    Changes made to connect to api

--- src/web/src/config.js
+++ src/web/src/config.js
@@ -1,25 +1,26 @@
-
-export const applicationName = "";
-export const applicationIcon = "mdi-cash-register";
+export const applicationName = '';
+export const applicationIcon = 'mdi-cash-register';
 export const hasSidebar = true;
 export const hasSidebarClosable = false;
 
 export const sections = [
-    {
-        name: "Dashboard",
-        url: "/",
-        icon: "mdi-view-dashboard"
-    },
-    {
-        name: "Basic Form",
-        url: "/form",
-        icon: "mdi-book-open-variant"
-    },
-    {
-        name: "Data grid",
-        url: "/grid",
-        icon: "mdi-table-large"
-    }
+	{
+		name: 'Dashboard',
+		url: '/',
+		icon: 'mdi-view-dashboard',
+	},
+	{
+		name: 'Basic Form',
+		url: '/form',
+		icon: 'mdi-book-open-variant',
+	},
+	{
+		name: 'Data grid',
+		url: '/grid',
+		icon: 'mdi-table-large',
+	},
 ];
 export const environment = process.env.NODE_ENV;
-export const apiBaseUrl = process.env.API_URL ? process.env.API_URL : "http://localhost:3000";
+export const apiBaseUrl = process.env.VUE_APP_API_URL
+	? process.env.VUE_APP_API_URL
+	: 'http://localhost:3000';
