fa43de4d82631c619fcd5f1a0f1edda93eddffcb
Ryan Agar <ryanjagar@hey.com>
2022-12-29 09:01:51 -0700

    setup

--- src/api/config.ts
+++ src/api/config.ts
@@ -3,19 +3,21 @@ import * as dotenv from "dotenv";
 let path;
 switch (process.env.NODE_ENV) {
   case "test":
-    path = `.env.test`;
+    path = `../env.test`;
     break;
   case "production":
-    path = `.env.production`;
+    path = `../../.env.production`;
     break;
   default:
-    path = `.env.development`;
+    path = `../../.env.development`;
 }
 dotenv.config({ path: path });
+console.log(`Using environment: ${process.env.NODE_ENV}`);
 
 export const API_PORT = parseInt(process.env.API_PORT || "3000");
 export const FRONTEND_URL = process.env.FRONTEND_URL || "";
-export const AUTH_REDIRECT = process.env.AUTH_REDIRECT || process.env.FRONTEND_URL || "";
+export const AUTH_REDIRECT =
+  process.env.AUTH_REDIRECT || process.env.FRONTEND_URL || "";
 export const NODE_ENV = process.env.NODE_ENV;
 
 export const DB_NAME = process.env.DB_NAME || "postgres";
