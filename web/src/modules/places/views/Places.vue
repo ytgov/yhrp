b380c5214d7266591d5f12f1818fe7ae6461dcec
Ryan Agar <ryanjagar@hey.com>
2023-03-23 14:38:47 -0700

    tidy places (needs api conncection)

--- web/src/modules/places/views/Places.vue
+++ web/src/modules/places/views/Places.vue
@@ -1,101 +1,86 @@
 <template>
-  <div>
-    <v-app-bar color="white" flat dark>
-      <v-toolbar-title class="black--text mr-2">
-        List of Historic Places {{ this.filterText }}
-        {{ this.photoCountText }}</v-toolbar-title
+  <v-app-bar color="white" flat class="pb-4">
+    <v-toolbar-title class="black--text mr-2">
+      List of Historic Places {{ this.filterText }}
+      {{ this.photoCountText }}</v-toolbar-title
+    >
+
+    <v-spacer></v-spacer>
+    <query-multi-select class="mr-5"></query-multi-select>
+  </v-app-bar>
+  <v-container>
+    <v-row v-if="!loading">
+      <v-col
+        v-for="(item, i) in placesList.slice(0, 6)"
+        :key="`photo-${i}`"
+        class="child-flex"
+        cols="4"
       >
-      <v-spacer></v-spacer>
-    </v-app-bar>
-    <v-container>
-      <v-row>
-        <v-col cols="12">
-          <v-card width="1000px">
-            <v-data-table :items="placesList" :headers="headers">
-            </v-data-table>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-    <v-container grid-list-xs>
-      <v-row v-if="!loading">
-        <v-col
-          v-for="(item, i) in placesList"
-          :key="`photo-${i}`"
-          class="child-flex"
-          cols="4"
-        >
-          <div style="min-height: 48px">{{ item.Community || "" }}</div>
-          <v-hover>
-            <template v-slot:default="{ hover }">
-              <v-card class="mx-auto">
+        <v-hover>
+          <template v-slot="{ isHovering, props }">
+            <v-card elevation="5" v-bind="props">
+              <v-card-title>{{ item.Community || "" }} </v-card-title>
+
+              <!-- <v-overlay absolute color="#036358"> -->
+
+              <v-card flat class="mx-auto">
                 <v-img
                   :src="photoURL(item.PlaceId)"
                   class="white--text align-end"
                   aspect-ratio="1"
+                  cover
                 >
                 </v-img>
 
                 <v-card-actions>
-                  <v-card-subtitle
-                    v-if="selectedSorter == 0"
-                    v-text="item.PrimaryName || ''"
-                  ></v-card-subtitle>
-                  <v-card-subtitle
-                    v-else
-                    v-text="item.PrimaryName || ''"
-                  ></v-card-subtitle>
+                  <v-card-subtitle v-if="selectedSorter == 0">
+                    {{ item.PrimaryName }}
+                  </v-card-subtitle>
+                  <v-card-subtitle v-else>
+                    {{ item.PrimaryName }}</v-card-subtitle
+                  >
                 </v-card-actions>
-
-                <v-fade-transition>
-                  <v-overlay v-if="hover" absolute color="#036358">
-                    <v-btn @click="handleClick(item)">View Place</v-btn>
-                  </v-overlay>
-                </v-fade-transition>
               </v-card>
-            </template>
-          </v-hover>
-        </v-col>
-      </v-row>
-      <v-row v-if="loading">
-        <div class="loading">Loading...</div>
-      </v-row>
-      <v-row class="mb-2" v-if="!loading">
-        <v-col>
-          <div class="text-center">
-            <v-pagination
-              v-model="page"
-              :length="numberOfPages"
-              :total-visible="5"
-            ></v-pagination>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+              <v-overlay
+                contained
+                :model-value="isHovering"
+                class="align-center justify-center"
+              >
+                <div>
+                  <v-btn @click="handleClick(item)">View Place</v-btn>
+                </div>
+              </v-overlay>
+            </v-card>
+          </template>
+        </v-hover>
+      </v-col>
+    </v-row>
+    <v-row v-if="loading">
+      <div class="loading">Loading...</div>
+    </v-row>
+    <v-row class="mb-2" v-if="!loading">
+      <v-col>
+        <div class="text-center">
+          <v-pagination
+            v-model="page"
+            :length="numberOfPages"
+            :total-visible="5"
+          ></v-pagination>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 import localData from "../data/places";
-import { VDataTable } from "vuetify/labs/VDataTable";
-import { createWebHistory } from "vue-router";
+import axios from "axios";
+import QueryMultiSelect from "../components/QueryMultiSelect.vue";
 
 export default {
-  name: "places",
-  components: {
-    VDataTable,
-  },
+  name: "HistoricPlaces",
+  components: { QueryMultiSelect },
   data: () => ({
-    headers: [
-      { title: "Name", key: "PrimaryName" },
-      { title: "Community", key: "Community" },
-      { title: "ID", key: "YHSIId" },
-      // { title: "Designations", key: "Designations" },
-      // { title: "RecognitionDate", key: "RecognitionDate" },
-      // { title: "EnglishTeaser", key: "EnglishTeaser" },
-      // { title: "FrenchTeaser", key: "FrenchTeaser" },
-      // { title: "PlaceId", key: "PlaceId" },
-      // { title: "PhotoURL", key: "PhotoURL" },
-    ],
+    trueThing: true,
 
     search: "",
     selectedSorter: 0,
@@ -155,7 +140,8 @@ export default {
       //Redirects the user to the site form
       this.$router.push({
         name: "placeView",
-        params: { id: value.id, name: value.primaryName },
+        // params: { id: value.id, name: value.primaryName },
+        params: { name: value.PlaceId },
       });
     },
     getDataFromApi() {
@@ -230,6 +216,9 @@ export default {
     },
   },
   computed: {
+    totalPlaces() {
+      return this.placesList.length;
+    },
     sortByName: function () {
       return (
         this.photos
