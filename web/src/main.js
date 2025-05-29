138e06d0ea2f0b8fba6583125bd1c7f8986391d3
Ryan Agar <ryanjagar@hey.com>
2025-05-29 16:00:07 -0700

    refactor: remove unused store files and Pinia dependency

--- web/src/main.js
+++ web/src/main.js
@@ -1,19 +1,16 @@
 // Components
 import App from "./App.vue";
 
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { createWebHistory } from "vue-router";
 import createRouter from "@/router";
+import { createApp } from "vue";
+import { createWebHistory } from "vue-router";
 
 // Plugins
 import { registerPlugins } from "@/plugins";
 
 const router = createRouter(createWebHistory());
-const pinia = createPinia();
 
 const app = createApp(App);
-app.use(pinia);
 app.use(router);
 
 registerPlugins(app);
