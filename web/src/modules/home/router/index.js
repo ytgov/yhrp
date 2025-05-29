ce73006a626e972a24bd3eb59a98ef59d64ba3d1
Ryan Agar <ryanjagar@hey.com>
2025-05-29 16:33:02 -0700

    fix: remove profile route from home router

--- web/src/modules/home/router/index.js
+++ web/src/modules/home/router/index.js
@@ -1,5 +1,3 @@
-import { authGuard } from "@auth0/auth0-vue";
-
 const routes = [
   {
     path: "",
@@ -8,17 +6,6 @@ const routes = [
       {
         path: "/dashboard",
         component: () => import("../views/Dashboard.vue"),
-        children: [
-          // {
-          //   path: "",
-          //   component: () => import("./components/someComponent.vue"),
-          // },
-        ],
-      },
-      {
-        path: "/profile",
-        component: () => import("../components/Profile.vue"),
-        beforeEnter: authGuard,
         children: [
           // {
           //   path: "",
