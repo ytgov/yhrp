4de34ce40af03c9822f46e3bb0f1f60a7dd859dc
Ryan Agar <ryanjagar@hey.com>
2025-05-30 17:19:45 -0700

    chore: update dependencies, resolve security issues, and update build config for latest Vuetify/Vite

--- src/web/src/plugins/vuetify.js
+++ src/web/src/plugins/vuetify.js
@@ -7,16 +7,16 @@
 // Styles
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
-import { VDataTable } from "vuetify/labs/VDataTable";
+// import { VDataTable } from "vuetify/labs/VDataTable";
 
 // Composables
 import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  components: {
-    VDataTable,
-  },
+  // components: {
+  //   VDataTable,
+  // },
   theme: {
     themes: {
       light: {
--- src/web/vite.config.js
+++ src/web/vite.config.js
@@ -3,8 +3,8 @@ import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 
 // Utilities
-import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +13,7 @@ export default defineConfig({
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
+      styles: { configFile: "src/styles/settings.scss" },
     }),
   ],
   define: { "process.env": {} },
