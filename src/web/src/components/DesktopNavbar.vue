9eb9482fc10d6589cc110fb3e326033ee95531c7
Ryan Agar <ryanjagar@hey.com>
2025-06-03 11:48:23 -0700

    feat: create responsive navbar components for mobile and desktop views

---
+++ src/web/src/components/DesktopNavbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app-bar
+    app
+    color="#fff"
+    flat
+    height="70"
+    style="left: 0; border-bottom: 3px #f3b228 solid"
+  >
+    <div>
+      <img src="/yukon.svg" class="logo" height="44" />
+    </div>
+    <div class="pl-10">
+      <router-link to="/" class="d-flex align-center text-decoration-none">
+        <v-toolbar-title>
+          <span class="font-weight-bold text-h5">{{ title }}</span>
+        </v-toolbar-title>
+      </router-link>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <div class="d-flex align-center">
+      <v-divider class="mr-5" vertical inset></v-divider>
+
+      <v-btn variant="text" size="large" color="primary">
+        <v-icon>mdi-dots-vertical</v-icon>
+        <NavMenu />
+      </v-btn>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+import { applicationName } from "@/config";
+import NavMenu from "./NavMenu.vue";
+
+export default {
+  name: "DesktopNavbar",
+  components: {
+    NavMenu,
+  },
+  computed: {
+    title() {
+      return applicationName;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.text-decoration-none {
+  text-decoration: none;
+  color: inherit;
+}
+
+.logo {
+  margin: -8px 0 0 30px;
+}
+</style>
